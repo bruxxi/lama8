10b5b43369a3fb12ff4d173f4d1e4674d65b7a32
bruxxi <bruna_nunciato@hotmail.com>
2021-08-30 11:41:48 -0300

    ok

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ app.use("/user", userRouter);
 app.use("/band", bandRouter)
 app.use("/concert", concertRouter)
 
-const server = app.listen(3000, () => {
+const server = app.listen(process.env.PORT ||3000, () => {
     if (server) {
       const address = server.address() as AddressInfo;
       console.log(`Servidor rodando em http://localhost:${address.port}`);
